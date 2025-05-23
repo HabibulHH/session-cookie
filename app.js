84a4bbf232b89dc7bc8f4500b6bf1dea4649c0de
HabibulHH <habibul.hasan.hira@gmail.com>
2025-05-23 20:46:39 +0600

    Added hash values

--- app.js
+++ app.js
@@ -1,46 +1,40 @@
 const express = require('express');
+const crypto = require('crypto');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const cookieParser = require('cookie-parser');
 
-const session = require('express-session');
-app.use(cookieParser());
-
-app.use(
-  session({
-    secret: 'signature', // Change this to a strong secret in production
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 900000, httpOnly: true }
-  })
-);
-
-
-app.post('/login', (req, res) => {
-  const { username } = req.body;
-  if (!username) return res.status(400).send('Username required');
-  req.session.username = username;
-  req.session.loggedIn = true;
-  res.send('Logged in with session');
+app.post('/signin', (req, res) => {
+  const { password , username} = req.body;
+  const data = {
+    username: username,
+    password: password
+  }
+  // check from database
+  if (!password) return res.status(400).send('Password required');
+  const hash = crypto.createHmac('sha256',"8273asdfb",data).digest('hex');
+  // hased  value 
+  res.send(`Hashed value: ${hash}`);
 });
 
-
 app.get('/protected', (req, res) => {
-  if (req.session.loggedIn) {
-    res.send(`Hello ${req.session.username}, you have access to this protected route!`);
+  const hash = req.body.hash; 
+  const data ={
+    username:"hira",
+    password:"1234"
+  }
+  const hash2 = crypto.createHmac('sha256',"8273asdfb",data).digest('hex');
+  if (hash === hash2) {
+    res.send(`Hello ${data.username}, you have access to this protected route!`);
   } else {
     res.status(401).send('Unauthorized');
   }
 });
 
-app.post('/logout', (req, res) => {
-  req.session.destroy(() => {
-    res.send('Logged out');
-  });
-});
+
 
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
